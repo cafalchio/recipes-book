da5ec3aeeae1c94f1faac80be12d27543e0f3afb
cafalchio <maolivei@tcd.ie>
2023-03-07 19:52:33 +0000

    added allowed hosts

--- recipesbook/settings.py
+++ recipesbook/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['recipes-book.herokuapp.com', "*"]
 
 
 # Application definition
