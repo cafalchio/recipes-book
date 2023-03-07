10fbea05a84b420af982c50fb4b114c066a697b1
cafalchio <maolivei@tcd.ie>
2023-03-07 19:39:01 +0000

    added allowed hosts

--- recipesbook/settings.py
+++ recipesbook/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recipes-book.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'recipesbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
