c75f75fafc0f398995d376fae8c15c8e3e37d69a
cafalchio <maolivei@tcd.ie>
2023-03-07 19:43:24 +0000

    added allowed hosts

--- recipesbook/settings.py
+++ recipesbook/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cafalchio.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cafalchio.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
