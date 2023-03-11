76eaf26370cdd1026a711bbf7cad963370a8bcab
cafalchio <maolivei@tcd.ie>
2023-03-11 19:18:05 +0000

    test deployment

--- recipesbook/settings.py
+++ recipesbook/settings.py
@@ -19,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['recipes-book.herokuapp.com', "*"]
 
