3359cfaca6e2cf622e3d679967fd2e0cd8453ec7
cafalchio <maolivei@tcd.ie>
2023-03-12 12:42:09 +0000

    added views and allowed hosts to settings

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from models import Recipe
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from recipes.forms import RecipeForm
+from django.shortcuts import redirect
 
-# Create your views here.
+class RecipeView(View):
+    # all public recipes
+    def get(self, request):
+        recipes = Recipe.objects.filter(is_public=True)
+        return render(request, 'recipes.html', {'recipes': recipes})
+    
+    def get(self, request, recipe_id):
+        recipe = Recipe.objects.get(id=recipe_id)
+        if recipe.is_public:
+            return render(request, 'recipe.html', {'recipe': recipe})
+        else:
+            return HttpResponseForbidden()  
+        
+    @login_required
+    def get(self, request):
+        recipes = Recipe.objects.filter(user=request.user)
+        return render(request, 'recipes.html', {'recipes': recipes})
+    
+    @login_required
+    def get(self, request, recipe_id):
+        recipe = Recipe.objects.get(id=recipe_id)
+        if recipe.user == request.user:
+            return render(request, 'recipe.html', {'recipe': recipe})
+        else:
+            return HttpResponseForbidden()
+   
+    @login_required
+    def delete(self, request, recipe_id):
+        recipe = Recipe.objects.get(id=recipe_id)
+        if recipe.user == request.user:
+            recipe.delete()
+            return redirect('recipes')
+        else:
+            return HttpResponseForbidden()
+    
+    @login_required
+    def post(self, request):
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
+            return redirect('recipes')
+        else:
+            return render(request, 'new_recipe.html', {'form': form})
--- recipesbook/settings.py
+++ recipesbook/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['recipes-book.herokuapp.com', "*"]
+ALLOWED_HOSTS = ['recipes-book.herokuapp.com', 'localhost']
 
 
 # Application definition
