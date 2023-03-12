09e71849a7d3aa0d38ba6bcf45867c92640467fa
cafalchio <maolivei@tcd.ie>
2023-03-12 23:13:49 +0000

    adding logout

--- web_page/views.py
+++ web_page/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     return render(request, 'web_page/home.html')
 
-def login(request):
-    return render(request, 'web_page/login.html')
+@login_required
+def logout(request):
+    logout(request)
+    return redirect('home')
