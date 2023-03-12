431cc0e582d6d1db35ebe4acb09eda7a5b86a852
cafalchio <maolivei@tcd.ie>
2023-03-12 11:45:11 +0000

    added test pages login and home

--- web_page/views.py
+++ web_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'web_site/home.html')
+    return render(request, 'web_page/home.html')
 
+def login(request):
+    return render(request, 'web_page/login.html')
