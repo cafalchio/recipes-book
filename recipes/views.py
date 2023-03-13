00b7a0c1fa44bc1b8c533aab843ec7c8cb63debd
cafalchio <maolivei@tcd.ie>
2023-03-13 19:55:55 +0000

    adding login and logout

--- recipes/views.py
+++ recipes/views.py
@@ -3,27 +3,37 @@ from django.http import HttpResponseForbidden
 from django.shortcuts import render
 from recipes.models import Recipe
 
-def get_all(request):
+def public_recipes(request):
     recipes = Recipe.objects.filter(is_public=True)
-    print(recipes)
     return render(request, 'recipes/recipes.html', {'recipes': recipes})
 
-def get(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
-    if recipe.is_public:
-        return render(request, 'recipe.html', {'recipe': recipe})
-    else:
-        return HttpResponseForbidden()  
-    
-@login_required
-def get_my_recipes(request):
-    recipes = Recipe.objects.filter(user=request.user)
-    return render(request, 'recipes.html', {'recipes': recipes})
+# def public_recipe(request, recipe_id):
+#     recipe = Recipe.objects.get(id=recipe_id)
+#     if recipe.is_public:
+#         return render(request, 'recipes/recipe.html', {'recipe': recipe})
+#     else:
+#         return HttpResponseForbidden()
 
-@login_required
-def get_my_recipe(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
-    if recipe.user == request.user:
-        return render(request, 'recipe.html', {'recipe': recipe})
-    else:
-        return HttpResponseForbidden()
+# @login_required
+# def get_my_recipes(request):
+#     recipes = Recipe.objects.filter(user=request.user)
+#     return render(request, 'recipes.html', {'recipes': recipes})
+
+# @login_required
+# def get_my_recipe(request, recipe_id):
+#     recipe = Recipe.objects.get(id=recipe_id)
+#     if request.method == 'POST':
+#         form = RecipeForm(request.POST, request.FILES, instance=recipe)
+#         if form.is_valid():
+#             form.save()
+#             return render(request, 'recipe.html', {'recipe': recipe})    
+#         return render(request, 'recipe.html', {'recipe': recipe})
+    
+#     elif request.method == 'GET':
+#         if recipe.user == request.user:
+#             return render(request, 'recipe.html', {'recipe': recipe})
+#         else:
+#             return HttpResponseForbidden()    
+        
+#     elif request.method == 'DELETE':
+#         pass
