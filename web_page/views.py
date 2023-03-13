ca01c613ea8d6d2bb8fff8a75ee3bc24d813592a
cafalchio <maolivei@tcd.ie>
2023-03-13 19:56:19 +0000

    added login view

--- web_page/views.py
+++ web_page/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'web_page/home.html')
 
+def login_page(request):        
+    return render(request, 'web_page/login.html')
+
 @login_required
 def logout_view(request):
     if request.method == 'POST':
