b6791316d42c0a189e67c83fcb30d4dec0bad362
cafalchio <maolivei@tcd.ie>
2023-03-14 19:37:48 +0000

    removed login logout views

--- web_page/views.py
+++ web_page/views.py
@@ -5,12 +5,3 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'web_page/index.html')
-
-def login_page(request):        
-    return render(request, 'web_page/login.html')
-
-@login_required
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('index')
