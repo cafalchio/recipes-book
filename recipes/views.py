f21e9baf545f6fc2fe7cb9f5e9334dac359571c7
cafalchio <maolivei@tcd.ie>
2023-03-18 14:21:32 +0000

    added comments and reformated the code

--- recipes/views.py
+++ recipes/views.py
@@ -5,13 +5,12 @@ from recipes.forms import RecipeForm
 from recipes.models import Recipe
 
 def public_recipes(request):
+    """List all public recipes"""
     recipes = Recipe.objects.filter(is_public=True)
     return render(request, 'recipes/recipes.html', {'recipes': recipes})
 
 def public_recipe(request, recipe_id):
-    """
-    Render selected public recipe detailed view page
-    """
+    """Render selected public recipe detailed view page"""
     recipe = get_object_or_404(Recipe, pk=recipe_id, is_public=True)
     context = {
         'recipe': recipe,
@@ -20,23 +19,24 @@ def public_recipe(request, recipe_id):
 
 @login_required
 def get_my_recipes(request):
+    """List all recipes for the authenticated user"""
     recipes = Recipe.objects.filter(user=request.user)
     return render(request, 'recipes/recipes.html', {'recipes': recipes})
 
 @login_required
 def get_my_recipe(request, recipe_id):
-    recipe = Recipe.objects.filter(user=request.user , id=recipe_id)
+    """Render detailed view for a specific recipe of the authenticated user"""
+    recipe = get_object_or_404(Recipe, user=request.user, id=recipe_id)
     return render(request, 'recipes/recipe.html', {'recipe': recipe})
 
 @login_required
-# tutorial from https://www.youtube.com/watch?v=CVEKe39VFu8 
 def add_recipe(request):
+    """Add a new recipe for the authenticated user"""
     if request.method == 'POST':
         form = RecipeForm(request.POST)
         if form.is_valid():
-            form.instance.user = request.user
-            form.save()
             recipe = form.save(commit=False)
+            recipe.user = request.user
             recipe.save()
             return redirect('my_recipes')
     else:
@@ -45,15 +45,20 @@ def add_recipe(request):
         
 @login_required     
 def delete_recipe(request, recipe_id):
-    print(f"REQUEST USER: {request.user}")
-    print(f"RECIPE USER: {recipe_id.user}")
-    if request.user != recipe_id.user:
-        return redirect('my_recipes')
-    recipe_id.delete()
+    """Delete a recipe for the authenticated user"""
+    recipe = get_object_or_404(Recipe, id=recipe_id, user=request.user)
+    recipe.delete()
+    return redirect('my_recipes')
 
 @login_required    
 def edit_recipe(request, recipe_id):
-    if request.user != recipe_id.user:
-        return redirect('my_recipes')
-
-        
+    """Edit a recipe for the authenticated user"""
+    recipe = get_object_or_404(Recipe, id=recipe_id, user=request.user)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('my_recipes')
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/edit_recipe.html', {'form': form})
